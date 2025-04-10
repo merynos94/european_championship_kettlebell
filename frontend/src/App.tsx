b2980eccc9e47c62600d9e38e0475d8d387803b0
merynos94 <45914039+merynos94@users.noreply.github.com>
2025-04-10 22:11:11 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CategoryPage from "./pages/CategoryPage";
+import LivePage from "./pages/LivePage";
+import ContactPage from "./pages/ContactPage";
 import Layout from "./components/Layout";
 import "./index.css";
 
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="category/:categoryId" element={<CategoryPage />} />
+          <Route path="live" element={<LivePage />} />
+          <Route path="contact" element={<ContactPage />} />
         </Route>
       </Routes>
     </Router>
