5dc89da5765c2b33221d618fa2fffcfad096da97
merynos94 <45914039+merynos94@users.noreply.github.com>
2025-04-14 16:42:08 +0200

    Update manage.py to choose right path to base setting

--- backend/european_champonship_kettlebell/manage.py
+++ backend/european_champonship_kettlebell/manage.py
@@ -7,7 +7,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "european_champonship_kettlebell.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "european_champonship_kettlebell.settings.base")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "european_champonship_kettlebell.settings")
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
