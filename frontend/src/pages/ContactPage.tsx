5b0cdff3d84401ce1a5f26e55e442534a8c14e25
merynos94 <45914039+merynos94@users.noreply.github.com>
2025-04-18 21:24:48 +0200

    Update Contact Info

--- frontend/src/pages/ContactPage.tsx
+++ frontend/src/pages/ContactPage.tsx
@@ -12,43 +12,36 @@ const ContactPage: React.FC = () => {
   return (
     <div>
       <Title level={1}>Kontakt</Title>
-      <Paragraph>
-        Masz pytania dotyczące zawodów, wyników lub strony? Skontaktuj się z
-        nami, korzystając z poniższych informacji.
-      </Paragraph>
+
 
       <Row gutter={[16, 16]}>
         <Col xs={24} md={12} lg={8}>
           <Card title="Organizator Główny" bordered={false}>
             <Paragraph>
-              <Text strong>Nazwa Organizacji/Stowarzyszenia</Text>
+              <Text strong>Ząbkowicka Szkoła Siły</Text>
               <br />
-              Ulica Przykładowa 123
-              <br />
-              00-001 Miasto, Kraj
-              <br />
+              Powstańców Warszawy 8i,<br />
+              57-200 Ząbkowice Śląskie.
             </Paragraph>
             <Paragraph>
               <MailOutlined />{" "}
-              <Link href="mailto:kontakt@example.com">kontakt@example.com</Link>
+              <Link href="mailto:kontakt@example.com">ckbzabkowice@op.pl</Link>
             </Paragraph>
             <Paragraph>
               <PhoneOutlined />{" "}
-              <Link href="tel:+48123456789">+48 123 456 789</Link>
+              <Link href="tel:+48722166895">Błazej Sobala</Link>
             </Paragraph>
           </Card>
         </Col>
 
         <Col xs={24} md={12} lg={8}>
-          <Card title="Sprawy Techniczne (Strona WWW)" bordered={false}>
+          <Card title="Facebook" bordered={false}>
             <Paragraph>
-              W przypadku problemów technicznych ze stroną wyników, prosimy o
-              kontakt:
+            MP Facebook
             </Paragraph>
             <Paragraph>
-              <MailOutlined />{" "}
-              <Link href="mailto:webmaster@example.com">
-                webmaster@example.com
+              <Link href="https://www.facebook.com/mphardstyle">
+                MP Hardstyle Kettlebell
               </Link>
             </Paragraph>
           </Card>
@@ -59,8 +52,8 @@ const ContactPage: React.FC = () => {
             <Paragraph>
               <EnvironmentOutlined /> Hala Sportowa "Arena Mistrzów"
               <br />
-              Aleja Olimpijska 1<br />
-              00-002 Miasto Sportowe
+              Powstańców Warszawy 8i,<br />
+              57-200 Ząbkowice Śląskie.
             </Paragraph>
           </Card>
         </Col>
